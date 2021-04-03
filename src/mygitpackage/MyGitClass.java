04468bc09762357af20aefbced30697dafc0a10a
Cemal-Arik <jamesjemas5555@gmail.com>
2021-04-03 18:56:52 +0200

    Fersion03

--- src/mygitpackage/MyGitClass.java
+++ src/mygitpackage/MyGitClass.java
@@ -5,7 +5,8 @@ public class MyGitClass {
 	public static void main(String[] args) {
 		
 		System.out.println("Version 1");
-
+		System.out.println("My second commit");
+		System.out.println("Hello I'm here");
 	}
 
 }
