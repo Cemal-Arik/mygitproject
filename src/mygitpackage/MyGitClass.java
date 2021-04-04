6e7d69a9bd2d1ccc7ac27193adae52f82631e5ef
Cemal-Arik <jamesjemas5555@gmail.com>
2021-04-04 19:06:50 +0200

    yeni versiyon

--- src/mygitpackage/MyGitClass.java
+++ src/mygitpackage/MyGitClass.java
@@ -6,7 +6,7 @@ public class MyGitClass {
 		System.out.println("Version 1");
 		System.out.println("My second commit");
 		System.out.println("Hello I'm here");
-<<<<<<< HEAD
+
 		System.out.println("My fourt commit");
 		System.out.println("Uzak sunucudaki kodlarimm"); //GitHub.com daki kodum
 		System.out.println("Takim arkadasim'in komutudur dikkat ");
@@ -16,7 +16,7 @@ public class MyGitClass {
 	
 	
 		System.out.println("17. satir bilgisayarim");
-=======
+
 		System.out.println("My 4th commit");
 		System.out.println("Uzak sunucudaki kodlarim"); //GitHub.com daki kodum
 		System.out.println("Takim arkadasim Bey'in komutudur"); //Github.com daki yeni kodum
@@ -24,11 +24,10 @@ public class MyGitClass {
 		System.out.println("12. SATIR bilgisayarim");
 		
 		System.out.println("13.satir UZAK SUNUCU"); //Github.com  daki yeni kodum
-<<<<<<< HEAD
->>>>>>> refs/remotes/origin/master
-=======
+
+
 		System.out.println("16.satir UZAK SUNUCU");
->>>>>>> refs/remotes/origin/master
+
 	}
 
 }
