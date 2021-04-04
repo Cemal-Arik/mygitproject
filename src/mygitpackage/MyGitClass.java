21819ef2fd418725df41d4e3697ac7bbed86bd59
Cemal ARIK <81856751+Cemal-Arik@users.noreply.github.com>
2021-04-04 18:49:14 +0200

    Update MyGitClass.java

--- src/mygitpackage/MyGitClass.java
+++ src/mygitpackage/MyGitClass.java
@@ -3,14 +3,17 @@ package mygitpackage;
 public class MyGitClass {
 
 	public static void main(String[] args) {
-		
 		System.out.println("Version 1");
 		System.out.println("My second commit");
 		System.out.println("Hello I'm here");
 		System.out.println("My 4th commit");
 		System.out.println("Uzak sunucudaki kodlarim"); //GitHub.com daki kodum
 		System.out.println("Takim arkadasim Bey'in komutudur"); //Github.com daki yeni kodum
+		
+		System.out.println("12. SATIR bilgisayarim");
+		
 		System.out.println("13.satir UZAK SUNUCU"); //Github.com  daki yeni kodum
+		System.out.println("16.satir UZAK SUNUCU");
 	}
 
 }
