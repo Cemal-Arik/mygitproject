b6c3700cf86a247a8b53fb95f163ec3857c50101
Cemal-Arik <jamesjemas5555@gmail.com>
2021-04-04 18:49:22 +0200

    yeni fersion

--- src/mygitpackage/MyGitClass.java
+++ src/mygitpackage/MyGitClass.java
@@ -3,13 +3,18 @@ package mygitpackage;
 public class MyGitClass {
 
 	public static void main(String[] args) {
-		
 		System.out.println("Version 1");
 		System.out.println("My second commit");
 		System.out.println("Hello I'm here");
 		System.out.println("My fourt commit");
-		System.out.println("Uzak sunucudaki kodlarim"); //GitHub.com daki kodum
-		System.out.println("Cemal Bey'in komutudur dikkat :) ");
+		System.out.println("Uzak sunucudaki kodlarimm"); //GitHub.com daki kodum
+		System.out.println("Takim arkadasim'in komutudur dikkat ");
+		
+		System.out.println("13.satir bilgisayarim ");
+	
+	
+	
+		System.out.println("17. satir bilgisayarim");
 	}
 
 }
